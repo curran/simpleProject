75770dfe35cf9767d2239b6e5f10139d0ffde30f
Curran <curran.kelleher@gmail.com>
2015-05-15 19:33:38 -0700

    Got simple project working

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # simpleProject
 A project that uses a simple plugin.
+
+This project was created with the following commands:
+
+```
+jspm init
+jspm install simplePlugin=github:curran/simplePlugin -o "{ main: 'main.js', format: 'amd' }"
+vim index.html
+```
---
+++ config.js
@@ -0,0 +1,15 @@
+System.config({
+  "baseURL": "/",
+  "transpiler": "traceur",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "simplePlugin": "github:curran/simplePlugin@master"
+  }
+});
+
